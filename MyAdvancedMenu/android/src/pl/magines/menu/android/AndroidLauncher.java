6e397b45c1c745ec347c8cdeb8d08c48dcc99853
Artur Mackowiak <artur.mackowiak@allegro.pl>
2015-02-27 17:31:42 +0100

    Fix

---
+++ MyAdvancedMenu/android/src/pl/magines/menu/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package pl.magines.menu.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import pl.magines.menu.MyAdvancedMenu;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyAdvancedMenu(), config);
+	}
+}
---
+++ MyAdvancedMenu/desktop/src/pl/magines/menu/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.magines.menu.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.magines.menu.MyAdvancedMenu;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyAdvancedMenu(), config);
+	}
+}
